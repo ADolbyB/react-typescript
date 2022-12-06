13b32419b667f33d7103e8f6d226ed136d8017a8
Joel Brigida <joel@drtechreps.com>
2022-12-05 23:58:40 -0500

    Completed Types and Objects

---
+++ 03-Basics-Objects/build/js/main.js
@@ -0,0 +1,63 @@
+"use strict";
+let stringArray = ["one", "hey", "Joel"]; // String[] type
+let guitars = ["Strat", "Les Paul", 5150]; // Union Type array
+let mixedData = ["EVH", 1984, true]; // Union type
+stringArray[0] = "42";
+stringArray.push("turd");
+guitars[0] = 1984;
+guitars.unshift("Jimmy"); // add array index
+let test = [];
+let bands = [];
+bands.push("Van Halen");
+// Tuple
+let myTuple = ["Joel", 40, false]; // tuple type with initialized data
+let mixed = ["Joel", 35, false]; // Union Type Array definition: NOT A TUPLE
+mixed = myTuple; // This is OK since the array variable accepts those types
+myTuple[1] = 55;
+myTuple[0] = "Andrew";
+myTuple[2] = true; // All legal assignments
+// Objects
+let myObject;
+myObject = [];
+console.log(typeof myObject);
+myObject = bands;
+myObject = {};
+const exampleObj = {
+    property1: "Joel",
+    property2: "Ashley",
+    property3: "Andrew"
+};
+exampleObj.property1 = "Baby Boy";
+let evh = {
+    name: "Eddie",
+    active: false,
+    albums: [1984, 5150, "OU812"]
+};
+let jp = {
+    name: "Jimmy",
+    albums: ["I", "II", "IV"]
+};
+let jb = {
+    active: false,
+    albums: ["None"]
+};
+//evh = jp // legal since they are equal types
+const greetGuitarist = (guitarist) => {
+    if (guitarist.name) {
+        return `Hello ${guitarist.name.toUpperCase()}!!`;
+    }
+    return "Hey-odiley-doodily!!!";
+};
+console.log(greetGuitarist(evh));
+console.log(greetGuitarist(jp));
+console.log(greetGuitarist(jb));
+// Enums:
+var Grade;
+(function (Grade) {
+    Grade[Grade["U"] = 5] = "U";
+    Grade[Grade["D"] = 6] = "D";
+    Grade[Grade["C"] = 7] = "C";
+    Grade[Grade["B"] = 8] = "B";
+    Grade[Grade["A"] = 9] = "A";
+})(Grade || (Grade = {}));
+console.log(Grade.U);
